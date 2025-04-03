2b0d66577dcd0db653ee11f2fe06126431ac957f
mfarhann10 <mfarhann1009@gmail.com>
2025-04-03 19:37:22 +0700

    test push git

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <h1>The Wild Oasis, Welcome to the paradise</h1>
-      <h2>Test Trackingsss</h2>
+      <h2>Test di build apa ga</h2>
     </div>
   );
 }
