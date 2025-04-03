19ab8f7165cbac3247dcbd86cff451689e4c3fb1
mfarhann10 <mfarhann1009@gmail.com>
2025-04-03 14:00:54 +0700

    test

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <h1>The Wild Oasis, Welcome to the paradise</h1>
-      <h2>Test Tracking</h2>
+      <h2>Test Trackingsss</h2>
     </div>
   );
 }
