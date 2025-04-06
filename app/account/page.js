c44aaeab4ba3acdc7a09d75d9481153edc5d3d3c
mfarhann10 <mfarhann1009@gmail.com>
2025-04-06 13:50:36 +0700

    adding nested routes and pages

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h3>Your Account</h3>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Farhan
+    </h2>
+  );
 }
