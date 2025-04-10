d946bd67c1ab43e23864b55581d4659ce015ecb9
mfarhann10 <mfarhann1009@gmail.com>
2025-04-10 14:21:52 +0700

    streaming UI with suspense

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,6 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
+import CabinList from "../_components/CabinList";
 
 export const metadata = {
   title: "Cabins",
@@ -7,7 +8,6 @@ export const metadata = {
 
 export default async function Page() {
   // CHANGE
-  const cabins = await getCabins();
 
   return (
     <div>
@@ -23,13 +23,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
--- app/loading.js
+++ app/loading.js
@@ -1,3 +1,5 @@
-export default function Loading(){
-    return <p>LOADING DATA...</p>
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loading() {
+  return <Spinner />;
 }
