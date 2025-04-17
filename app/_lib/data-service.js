77f0dd470890f273ca9c9a7b85cfa4b4927b170f
mfarhann10 <mfarhann1009@gmail.com>
2025-04-17 12:07:58 +0700

    fetching data and suspense these two comp in cabinId

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -127,11 +127,12 @@ export async function getBookedDatesByCabinId(cabinId) {
 }
 
 export async function getSettings() {
-  const { data, error } = await supabase.from('settings').select('*').single();
+  const { data, error } = await supabase.from("settings").select("*").single();
 
+  // await new Promise((res) => setTimeout(res, 5000));
   if (error) {
     console.error(error);
-    throw new Error('Settings could not be loaded');
+    throw new Error("Settings could not be loaded");
   }
 
   return data;
