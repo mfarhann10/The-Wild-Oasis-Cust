8237b71c275bdb3a86388f937636096595933114
mfarhann10 <mfarhann1009@gmail.com>
2025-04-30 21:06:29 +0700

    add useOptimistic hooks into a delete reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -71,6 +71,8 @@ export async function editReservation(formData) {
 }
 
 export async function deleteReservation(bookingId) {
+  await new Promise((res) => setTimeout(res, 2000));
+
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
